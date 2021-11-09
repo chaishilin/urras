6e11b9e7596d27645a3de6cd700aa1349be8f9f9
chaishilin <592654362@qq.com>
2021-11-09 10:35:59 +0800

    删除模板中的中问注释

--- src/views/doProgramPage.vue
+++ src/views/doProgramPage.vue
@@ -104,7 +104,7 @@ export default {
       getSave: false,
       canDelete: false,
       supportLanguageList: [],
-      code: "public int yourFunction(int a,int b){\n        //示例：~求数字区间[a,b]的和\n        int sum = 0;\n        for(int i = a;i<=b;i++){\n            sum += i;\n        }\n        return sum;\n    }\n",
+      code: "public int yourFunction(int a,int b){\n        int sum = 0;\n        for(int i = a;i<=b;i++){\n            sum += i;\n        }\n        return sum;\n    }\n",
       language: "java",
       editorOption: {},
       programInfo: {
